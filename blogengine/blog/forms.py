765e94a666b56aa98a8ea86b7d05e39875b7bd05
bektursun <begaliev0020>
2020-02-17 02:43:26 +0600

    molchanov django

---
+++ blogengine/blog/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Tag, Post
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ["title", "slug"]
+
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "slug": forms.TextInput(attrs={"class": "form-control"})
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError("slug may not be 'create'")
+        if Tag.objects.filter(slug__iexact=new_slug).count():
+            raise ValidationError(f"slug must be unique. we have '{new_slug}' slug already")
+        return new_slug
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "slug", "body", "tags"]
+
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "slug": forms.TextInput(attrs={"class": "form-control"}),
+            "body": forms.TextInput(attrs={"class": "form-control"}),
+            "tags": forms.SelectMultiple(attrs={"class": "form-control"}),
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data["slug"].lower()
+
+        if new_slug == "create":
+            raise ValidationError("slug may not be 'create'")
+        return new_slug
