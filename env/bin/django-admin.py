4cb9b00c16ffab8bc1c8a691c13fd35e89c778e3
Iskender <kurmanov.iskender@gmail.com>
2020-03-10 16:01:48 +0600

    final

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/isken/Documents/bootcamp/lessons/molchanov_django/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
